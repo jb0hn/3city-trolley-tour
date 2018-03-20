1dd2da7667f94e75d3e2f2dc082633a97e5e1d77
Józef Bongowski <j.bongowski@protonmail.com>
2018-03-20 19:08:51 +0100

    First commit

---
+++ 3city-travel.py
@@ -0,0 +1,113 @@
+import os, csv
+
+class Player(object):
+    def __init__(self, name, position=0):
+        self.name = name
+        self.position = position
+
+    def __str__(self):
+        return self.name
+
+    def change_position(self, new_position):
+        self.position = new_position
+
+
+class Place(object):
+    def __init__(self, name):
+        self.name = name
+        self.neighbor_list = []
+
+    def __str__(self):
+        return self.name
+
+    def add_neighbor(self, new_neighbor):
+        self.neighbor_list.append(new_neighbor)
+
+
+class Game(object):
+    def __init__(self, name):
+        self.player = Player(name)
+
+        STATIONS = []
+
+        # import stations list from csv file
+        with open("3city_stations.csv") as skm_station_file:
+            reader = csv.reader(skm_station_file)
+            skm_station_list = list(reader)
+
+        # transform nested, two-dim list into one dimentional
+        for station in skm_station_list:
+            STATIONS.append(station[0])
+
+        self.places = []
+        for city_name in STATIONS:
+            place = Place(city_name)
+            self.places.append(place)
+
+    def what_neighbour(self):
+        for place in self.places:
+            if self.places.index(place) not in [0, len(self.places) - 1]:
+                place.add_neighbor(self.places[self.places.index(place) - 1])
+                place.add_neighbor(self.places[self.places.index(place) + 1])
+            elif self.places.index(place) > 0:
+                place.add_neighbor(self.places[self.places.index(place) - 1])
+            else:
+                place.add_neighbor(self.places[self.places.index(place) + 1])
+
+    def travel(self):
+        neighborhood = []
+
+        print("\nYou're actually in ", end='')
+        print(self.places[self.player.position], ".", sep='')
+        print("\nYou can go to: ")
+        for neighbor in self.places[self.player.position].neighbor_list:
+            print(" -> ", neighbor, sep='')
+
+        while True:
+            destination = input("\nWhere do you wanna to go?: ")
+            for neighbor in self.places[self.player.position].neighbor_list:
+                neighborhood.append(neighbor.name)
+
+            if destination in neighborhood:
+                break
+
+        for place in self.places:
+            if destination == place.name:
+                new_position = self.places.index(place)
+        self.player.change_position(new_position)
+        print("\nYou arrived:", self.places[self.player.position])
+
+    def play(self):
+        self.what_neighbour()
+        self.travel()
+        self.places[self.player.position].neighbor_list.clear()
+
+# def name():
+#     return name
+
+def intro(name):
+    with open("intro.txt", "r") as intro_file:
+        header = intro_file.readline()
+        body = intro_file.read()
+    intro_file.close()
+
+    print(header)
+    print("Hello ", name, "!", sep='')
+    print(body)
+
+def main():
+    # TEMP: There'll be a introduction
+    # name = name()
+    name = input("\nWhats your name?: ")
+    os.system("clear")
+    intro(name)
+    game = Game(name)
+
+    again = None
+    while again != "n":
+        game.play()
+        again = input("\nDo you still want to travel? [Y/N]: ")
+
+
+main()
+input("\n\nTo exit, press Enter.")
